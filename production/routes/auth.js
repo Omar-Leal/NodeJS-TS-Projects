6e035421291c7c3b85c1ab2cc4a7950415dd1531
Omar Leal | Front-end Developer <omarenriquelealr@gmail.com>
2021-12-11 14:21:00 -0400

    gitignore

--- production/routes/auth.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = require("express");
-const authRoute = (0, express_1.Router)();
-authRoute.get('/', (req, res) => {
-    res.send('auth route created');
-});
-exports.default = authRoute;
-//# sourceMappingURL=auth.js.map
